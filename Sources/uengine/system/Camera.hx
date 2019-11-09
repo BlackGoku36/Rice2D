1ccaefcf1918ddcb53058a8cd0c8110b25954ebd
BlackGoku36 <urjasvisuthar@gmail.com>
2019-11-10 03:20:48 +0530

    remove culling

--- Sources/uengine/system/Camera.hx
+++ Sources/uengine/system/Camera.hx
@@ -11,13 +11,6 @@ class Camera {
 
     public function new() {
         transform = FastMatrix3.identity();
-        App.notifyOnUpdate(
-            function (){
-                for (object in Scene.objects) {
-                    checkCull(object);
-                }
-            }
-        );
     }
 
     public function set(g:Graphics) {
@@ -27,18 +20,6 @@ class Camera {
 
     public function unset(g:Graphics) {
         g.popTransformation();
-    }
-
-    public function checkCull(obj:Object){
-        var props = obj.props;
-        var a:Bool; var b:Bool;
-        if (props.x < x) a = x < props.x + props.width;
-		else a = props.x < x + Window.get(0).width;
-		if (props.y < y) b = y < props.y + props.height;
-		else b = props.y < y + Window.get(0).height;
-
-        props.culled = a && b;
-        // if(!props.culled) trace(props.name+" culled");
     }
 
 }
