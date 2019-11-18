61424e710059ae2c362d163d08d8b56cf324fec2
BlackGoku36 <urjasvisuthar@gmail.com>
2019-11-19 01:29:30 +0530

    Particle sprite

--- Sources/rice2d/Particle.hx
+++ Sources/rice2d/Particle.hx
@@ -1,5 +1,6 @@
 package rice2d;
 
+import kha.Image;
 import kha.graphics2.Graphics;
 import rice2d.data.ParticleData;
 
@@ -15,7 +16,9 @@ class Emitter {
     public function spawn() {
         for(i in 0...props.amount){
             var newParticle:Particle = new Particle(props.x, props.y,{
-                lifeTime: props.particle.lifeTime, speed: props.particle.speed, 
+                width: props.particle.width, height: props.particle.height,
+                lifeTime: props.particle.lifeTime, speed: props.particle.speed,
+                spriteRef: props.particle.spriteRef,
                 rots: props.particle.rots, rote:props.particle.rote
             });
             particles.push(newParticle);
@@ -38,10 +41,14 @@ class Particle {
     public var props: ParticleData;
     public var deltaTime: Float;
     public var rotataion: Float;
+
+    public var sprite:Image;
+
     public function new(x:Float, y:Float, particle:ParticleData) {
         deltaTime = 0.0;
         this.x = x;
         this.y = y;
+        this.sprite = getImage(particle.spriteRef);
         rotataion = randomRangeFloat(particle.rots, particle.rote);
         this.props = particle;
     }
@@ -50,18 +57,25 @@ class Particle {
         deltaTime += 1/60;
         if(props.lifeTime > deltaTime){
             x += props.speed * Math.cos(rotataion);
-            y += -props.speed * Math.sin(rotataion);
+            y += -props.speed * Math.sin(rotataion) *2;
         }
     }
 
     public function render(g:Graphics) {
         if(props.lifeTime > deltaTime){
-            //TODO: Add more types, visibility and size as it age.
-            g.fillRect(x, y, 5, 5);
+            g.drawScaledImage(sprite, Math.round(x - (props.width / 2)), Math.round(y - (props.height / 2)), props.width, props.height);
         }
     }
 
     public static function randomRangeFloat(min:Float, max:Float):Float {
 		return Math.random() * (max - min) + min;
 	}
+
+    function getImage(ref:String):Image {
+        var image:Image = null;
+        for (i in Scene.assets){
+            if(i.exists(ref)) image = i.get(ref);
+        }
+        return image;
+    }
 }
--- Sources/rice2d/data/ParticleData.hx
+++ Sources/rice2d/data/ParticleData.hx
@@ -1,8 +1,11 @@
 package rice2d.data;
 
 typedef ParticleData = {
+    var width:Float;
+    var height:Float;
     var lifeTime: Float;
     var speed: Float;
+    var spriteRef: String;
     var rots: Float;
     var rote: Float;
 }
