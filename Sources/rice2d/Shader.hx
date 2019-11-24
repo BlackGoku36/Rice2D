e6f90b401cfa4fcc526c6e6f3d6c8c8626e49cf3
BlackGoku36 <urjasvisuthar@gmail.com>
2019-11-24 16:11:03 +0530

    Clean up

--- Sources/rice2d/Shader.hx
+++ Sources/rice2d/Shader.hx
@@ -1,29 +1,17 @@
 package rice2d;
 
-import kha.graphics4.VertexShader;
-import kha.Scheduler;
 import kha.Canvas;
-import kha.graphics4.ConstantLocation;
-import kha.math.Vector4;
-import kha.math.Vector2;
-import kha.graphics4.BlendingFactor;
-import kha.Shaders;
-import kha.graphics4.VertexStructure;
-import kha.graphics4.FragmentShader;
 import kha.graphics4.PipelineState;
+import kha.graphics4.FragmentShader;
+import kha.graphics4.VertexShader;
+import kha.graphics4.VertexStructure;
 import kha.graphics4.VertexData;
+import kha.graphics4.BlendingFactor;
 
 class Shader {
     var pipeline:PipelineState;
-    var fragmentShader: FragmentShader;
-    var vertexShader: VertexShader;
-    public var timeID: ConstantLocation;
-
-    var constantFloat: Array<Map<String,Float>> = [];
-    var constantInt: Array<Map<String,Int>> = [];
-    var constantVec2: Array<Map<String,Vector2>> = [];
-    var constantVec3: Array<Map<String,Vector4>> = [];
-    var constantVec4: Array<Map<String,Vector4>> = [];
+    public var fragmentShader: FragmentShader;
+    public var vertexShader: VertexShader;
 
     public function new(shaderData: rice2d.data.ShaderData) {
         fragmentShader = shaderData.fragmentShader;
@@ -35,7 +23,7 @@ class Shader {
         var vertexStructure = new VertexStructure();
         vertexStructure.add("vertexPosition", VertexData.Float3);
         vertexStructure.add('vertexColor', VertexData.Float4);
-        // vertexStructure.add('texturePosition', VertexData.Float2);
+        vertexStructure.add('texturePosition', VertexData.Float2);
 
         pipeline.inputLayout = [vertexStructure];
 
@@ -45,23 +33,15 @@ class Shader {
         pipeline.alphaBlendDestination = BlendingFactor.InverseSourceAlpha;
 
         pipeline.compile();
+    }
 
-        // for (constant in shaderData.constants){
-        //     switch (constant.type){
-        //         case Float: constantFloat.push([constant.name => constant.value[0]]);
-        //         case Int: constantInt.push([constant.name => Std.int(constant.value[0])]);
-        //         case Vec2: constantVec2.push([constant.name => new Vector2(constant.value[0], constant.value[1])]);
-        //         case Vec3: constantVec3.push([constant.name => new Vector4(constant.value[0], constant.value[1], constant.value[2])]);
-        //         case Vec4: constantVec4.push([constant.name => new Vector4(constant.value[0], constant.value[1], constant.value[2], constant.value[3])]);
-        //     }
-        // }
-        timeID = pipeline.getConstantLocation("time");
+    public function getPipeline() {
+        return pipeline;
     }
 
     public function begin(canvas:Canvas){
         canvas.g2.pipeline = pipeline;
         pipeline.set();
-        canvas.g4.setFloat(timeID, Scheduler.time());
     }
 
     public function end(canvas:Canvas){
