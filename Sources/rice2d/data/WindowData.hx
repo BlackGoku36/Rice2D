bf766f203939e0622a6dfffe98400b28dd139989
BlackGoku36 <urjasvisuthar@gmail.com>
2019-11-28 16:05:51 +0530

    check all WindowData vaules

--- Sources/rice2d/data/WindowData.hx
+++ Sources/rice2d/data/WindowData.hx
@@ -14,7 +14,9 @@ class Window {
     public static function loadWindow(done: Void->Void) {
         kha.Assets.loadBlobFromPath("window.json", function (b:kha.Blob) {
             window = haxe.Json.parse(b.toString());
-            if(window.width == null || window.height == null) trace("Warning: All windows field aren't initialised, it will default to value given by kha.\n");
+            if(window.width == null || window.height == null || window.windowMode == null || window.clearColor == null){ 
+                trace("Warning: All windows field aren't initialised, it will default to value given by kha.\n");
+            }
             done();
         }, function(err: kha.AssetError){
             trace(err.error+". Make sure 'window.json' exist in 'Assets' folder.\n");
