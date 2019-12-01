e832e0488555e9ae3603874ba891c2cd6a9c8f4e
BlackGoku36 <urjasvisuthar@gmail.com>
2019-12-01 12:30:17 +0530

    Seperate window from WindowData

--- Sources/rice2d/data/WindowData.hx
+++ Sources/rice2d/data/WindowData.hx
@@ -7,19 +7,3 @@ typedef WindowData = {
     var windowMode: Int;
     var clearColor: Array<Int>;
 }
-
-class Window {
-    public static var window:WindowData;
-
-    public static function loadWindow(done: Void->Void) {
-        kha.Assets.loadBlobFromPath("window.json", function (b:kha.Blob) {
-            window = haxe.Json.parse(b.toString());
-            if(window.width == null || window.height == null || window.windowMode == null || window.clearColor == null){ 
-                trace("Warning: All windows field aren't initialised, it will default to value given by kha.\n");
-            }
-            done();
-        }, function(err: kha.AssetError){
-            trace(err.error+". Make sure 'window.json' exist in 'Assets' folder.\n");
-        });
-    }
-}
