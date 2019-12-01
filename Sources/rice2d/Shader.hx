db9a90ceb78438e6299daa928b3ff2d7423db161
BlackGoku36 <urjasvisuthar@gmail.com>
2019-12-01 14:41:00 +0530

    Make shaders data driven and use single render funtion

--- Sources/rice2d/Shader.hx
+++ Sources/rice2d/Shader.hx
@@ -1,17 +1,26 @@
 package rice2d;
 
+//Kha
 import kha.Canvas;
-import kha.graphics4.PipelineState;
+import kha.math.FastVector2;
 import kha.graphics4.FragmentShader;
 import kha.graphics4.VertexShader;
+import kha.graphics4.PipelineState;
 import kha.graphics4.VertexStructure;
 import kha.graphics4.VertexData;
 import kha.graphics4.BlendingFactor;
+import kha.graphics4.ConstantLocation;
+
+//Engine
+import rice2d.data.ShaderData;
+import rice2d.data.ShaderData.ConstantData;
 
 class Shader {
     var pipeline:PipelineState;
     public var fragmentShader: FragmentShader;
     public var vertexShader: VertexShader;
+    public var constants: Array<ConstantData>;
+    public var constantsLocations:Array<Map<ConstantLocation, ConstantData>> = [];
 
     public function new(shaderData: rice2d.data.ShaderData) {
         fragmentShader = shaderData.fragmentShader;
@@ -33,18 +42,38 @@ class Shader {
         pipeline.alphaBlendDestination = BlendingFactor.InverseSourceAlpha;
 
         pipeline.compile();
+        constants = shaderData.constants;
+        for (constant in shaderData.constants){
+            constantsLocations.push([ pipeline.getConstantLocation(constant.name) => constant]);
+        }
     }
 
     public function getPipeline() {
         return pipeline;
     }
 
-    public function begin(canvas:Canvas){
-        canvas.g2.pipeline = pipeline;
-        pipeline.set();
+    public function render(canvas:Canvas, render:Void->Void) {
+        begin(canvas);
+        render();
+        end(canvas);
     }
 
-    public function end(canvas:Canvas){
+    function begin(canvas:Canvas){
+        canvas.g2.pipeline = pipeline;
+        pipeline.set();
+        for (map in constantsLocations) {
+            for (cl => cd in map) {
+                switch (cd.type){
+                    case Int: canvas.g4.setInt(cl, Std.int(cd.val[0]));
+                    case Float: canvas.g4.setFloat(cl, cd.val[0]);
+                    case Bool: canvas.g4.setBool(cl, cd.bool);
+                    case Vec2: canvas.g4.setVector2(cl, new FastVector2(cd.val[0], cd.val[1]));
+                }
+            }
+        }
+    }
+
+    function end(canvas:Canvas){
         canvas.g2.pipeline = null;
     }
 }
--- Sources/rice2d/data/ShaderData.hx
+++ Sources/rice2d/data/ShaderData.hx
@@ -3,4 +3,19 @@ package rice2d.data;
 typedef ShaderData = {
     var vertexShader: kha.graphics4.VertexShader;
     var fragmentShader: kha.graphics4.FragmentShader;
+    var constants:Array<ConstantData>;
+}
+
+typedef ConstantData = {
+    var name:String;
+    var val:Array<Float>;
+    var ?bool:Bool;
+    var type:ConstantType;
+}
+
+@:enum abstract ConstantType(Int) from Int to Int {
+    var Int = 0;
+    var Float = 1;
+    var Bool = 2;
+    var Vec2 = 3;
 }
