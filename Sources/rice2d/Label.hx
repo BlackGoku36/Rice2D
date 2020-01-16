1c429dbfe4703d9075653ae62d2b8695e627ebed
BlackGoku36 <urjasvisuthar@gmail.com>
2020-01-17 00:05:28 +0530

    Improve Label

--- Sources/rice2d/Label.hx
+++ Sources/rice2d/Label.hx
@@ -4,9 +4,17 @@ import rice2d.data.ObjectData;
 
 class Label {
 
-	public static function hasLabel(objectData:ObjectData, hasLabel:String):Bool {
+	public static function hasLabel(objectData:ObjectData, label:String):Bool {
 		var has:Bool = false;
-		for(label in objectData.labels) if (label == hasLabel) has = true;
+		for(objLabel in objectData.labels) if (objLabel == label) has = true;
 		return has;
+	}
+
+	public static function getObjectsOfLabel(label:String):Array<Object> {
+		var objects:Array<Object> = [];
+		for(object in Scene.objects)
+			for(objLabel in object.props.labels)
+				if(objLabel == label) objects.push(object);
+		return objects;
 	}
 }
