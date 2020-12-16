356bb55d023cac6e32aea049f2b495cde55790e0
BlackGoku36 <urjasvisuthar@gmail.com>
2020-12-16 16:23:51 +0530

    Version 2021 wip

--- khafile.js
+++ khafile.js
@@ -1,8 +1,6 @@
 let project = new Project('Rice2D');
 project.addSources('Sources');
 project.addAssets('Assets/**');
-project.addShaders('Shaders/**', { defines: ['decoy'] });
-project.addLibrary('zui');
+project.addShaders('Shaders/**');
 project.addDefine('analyzer-optimize');
-project.addDefine('rice_postprocess');
 resolve(project);
